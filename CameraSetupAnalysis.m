9b316ca163c15b14ca5646e38cbc395d4a169b18
Joel Berkson <31609467+jdberkson@users.noreply.github.com>
2021-01-19 10:11:34 -0700

    Update CameraSetupAnalysis.m

--- CameraSetupAnalysis.m
+++ CameraSetupAnalysis.m
@@ -7,9 +7,9 @@ detV = 5.85*10^-3;
 % z = .5;
 % f = .010;
 % b = 2;
-z = .5;
-f = .012;
-b = 2.5;
+z = 1.5;
+f = .05;
+b = 1;
 
 FOVh = atand(detH/f);
 FOVv = atand(detV/f);
@@ -21,4 +21,4 @@ theta = atand(z/(b/2));
 x = 2*z_proj*tand(FOVh)
 y = 2*z_proj*tand(FOVv)/cosd(theta)
 
-dz = z^2/(b*f)*.000016*10^6*.5
+dz = z^2/(b*f)*.000016*10^6*.25;
